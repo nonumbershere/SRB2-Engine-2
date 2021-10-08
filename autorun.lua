dbe69c426375d2a89e93d265bc5bc04ac1ec89ea
Lapide <64395933+nonumbershere@users.noreply.github.com>
2021-10-08 01:28:16 -0400

    Update autorun.lua

--- autorun.lua
+++ autorun.lua
@@ -1,2 +1,2 @@
 -- autorun
-showMessage("Hello! This is a brand new version of SRB2 Engine. This is for version 2.2.9. Thanks for using it!")
+showMessage("Hello! This is a brand new version of SRB2 Engine. This is for version 2.2.9. Thanks for using it! - lapide")
